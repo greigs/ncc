6cbb688fe97d2f28d736273a3ed70977d9ffa53f
greigs <>
2019-10-30 00:16:04 +0000

    correct format for nes

--- examples/matrix.js
+++ examples/matrix.js
@@ -28,7 +28,7 @@ const createMatrix = () => {
     {
         ...LedMatrix.LedMatrix.defaultMatrixOptions(),
         rows: 64,
-        cols: 384,
+        cols: 256,
         chainLength: 1,
         hardwareMapping: LedMatrix.GpioMapping.AdafruitHatPwm,
         pixelMapperConfig: LedMatrix.LedMatrixUtils.encodeMappers({ type: LedMatrix.PixelMapperType.U }),
